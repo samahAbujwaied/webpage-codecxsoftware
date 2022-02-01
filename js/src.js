4786c743594117de10d7fa3d3761207478f8a3e2
‘samahAbujwaied’ <samah.abujweed@yahoo.com>
2022-02-01 14:21:26 +0200

    bulid a webpage

--- js/src.js
+++ js/src.js
@@ -1,4 +1,4 @@
-
+'use strict'
 
 let language = document.getElementById('lang');
 let paragraph1 = document.getElementById('paragraph1');
@@ -16,46 +16,55 @@ let sitemap2=document.getElementById('sitemap2');
 let newsfooter = document.getElementById('news-footer');
 let aboutUsfooter = document.getElementById('aboutus-footer');
 let contactus = document.getElementById('contactus-footer');
-
-let lang= 'arabic'
+let navbar = document.getElementById('navbar');
+let footer = document.getElementById('footer');
+let navflex = document.getElementById('navflex');
+let myNavbar = document.getElementById('myNavbar');
 let home = document.getElementById('home');
 let more = document.getElementById('more');
 let news =document.getElementById('news');
 let aboutUs = document.getElementById('aboutUs');
 let contactUs= document.getElementById('contactUs');
 let translate= document.getElementById('translate');
+let logo = document.getElementById('logo')
+let navitem = document.getElementById('navitem');
+let navlang = document.getElementById('navlang')
+let lang= 'arabic'
+
 language.onclick = ()=>{
     setLanguage(lang)
 }
 function setLanguage(getlang){
     if(getlang=='arabic'){
-        paragraph1.innerHTML='  هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل  هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل ';
+        paragraph1.innerHTML='هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل  هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل ';
         paragraph1.style.textAlign="right"
-        paragraph2.innerHTML='  هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل  هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل ';
+        paragraph2.innerHTML='هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل  هذه الفقرة الاولى ، الفقرة الثانية في الأسفل هذه الفقرة الاولى ، الفقرة الثانية في الأسفل ';
         paragraph2.style.textAlign="right"
         home.innerHTML='الصفحة الرئيسية';
         
         more.innerHTML='المزيد';
         news.innerHTML='الاخبار';
-        aboutUs.innerHTML='نبذة عنا '
-        contactUs.innerHTML='تواصل معنا'
-        translate.innerHTML='الإنجليزية'
-        contact.innerHTML='اتصل'
-        poBox.innerHTML='مكتب البريد'
-        phone.innerHTML='الهاتف'
-        fax.innerHTML='الفاكس'
-        locate.innerHTML='الموقع'
-        sitemap1.innerHTML='خريطة الموقع'
-        homefooter.innerHTML='الصفحة الرئيسية'
-        morefooter.innerHTML='المزيد'
-        readfooter.innerHTML='اقرأ'
-        sitemap2.innerHTML='خريطة الموقع'
-        newsfooter.innerHTML='الأخبار'
-        aboutUsfooter.innerHTML='عنا'
-        contactus.innerHTML='تواصل معنا'
+        aboutUs.innerHTML='نبذة عنا ';
+        contactUs.innerHTML='تواصل معنا';
+        translate.innerHTML='الإنجليزية';
+        contact.innerHTML='اتصل';
+        poBox.innerHTML='مكتب البريد';
+        phone.innerHTML='الهاتف';
+        fax.innerHTML='الفاكس';
+        locate.innerHTML='الموقع';
+        sitemap1.innerHTML='خريطة الموقع';
+        homefooter.innerHTML='الصفحة الرئيسية';
+        morefooter.innerHTML='المزيد';
+        readfooter.innerHTML='اقرأ';
+        sitemap2.innerHTML='خريطة الموقع';
+        newsfooter.innerHTML='الأخبار';
+        aboutUsfooter.innerHTML='عنا';
+        contactus.innerHTML='تواصل معنا';
+        navbar.style.direction='rtl';
+        myNavbar.style.direction='rtl';
+        footer.style.direction='rtl';
 
         lang='english'
-        console.log('laaan',lang);
     }
     else if (getlang=='english')
     {
@@ -63,7 +72,7 @@ function setLanguage(getlang){
         paragraph1.style.textAlign="left"
         paragraph2.innerHTML='This is a paragraph one , and the paragraph two is below this one ,This is a paragraph one , and the paragraph two is below this one ,This is a paragraph one , and the paragraph two is below this one ,This is a paragraph one , and the paragraph two is below this one ,This is a paragraph one , and the paragraph two is below this one ,This is a paragraph one , and the paragraph two is below this one ,This is a paragraph one , and the paragraph two is below this one ,This is a paragraph one , and the paragraph two is below this one ,This is a paragraph one , and the paragraph two is below this one ,This is a paragraph one , and the paragraph two is below this one';
         paragraph2.style.textAlign="left"
-        home.innerHTML='Main'
+        home.innerHTML='Home'
         more.innerHTML='More'
         news.innerHTML='News'
         aboutUs.innerHTML='About us'
@@ -82,7 +91,10 @@ function setLanguage(getlang){
         newsfooter.innerHTML='News'
         aboutUsfooter.innerHTML='About Us'
         contactus.innerHTML='Contact Us'
-
+        navbar.style.direction='ltr';
+        myNavbar.style.direction='ltr';
+        footer.style.direction='ltr';
+        
         lang='arabic'
     }
 
